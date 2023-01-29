521b09e5d4c6394f788d0056d4e89e92a27e904c
Juliano Kessler <juliano.kessler@bairesdev.com>
2023-01-29 17:20:45 -0300

    added snowflake remark

--- workstation.sh
+++ workstation.sh
@@ -3,5 +3,10 @@ apt get update
 apt get upgrade -y 
 apt get install filezilla -y
 
+# Snowflake (ftp e scp client)
+# https://github.com/subhra74/snowflake
+# Dowwnload file, then: 
+# sudo apt install ./snowflake-1.0.4-setup-amd64.deb
+
 # install virtualbox?
 # sudo apt-get install virtualbox (untested, but this was the command). 
