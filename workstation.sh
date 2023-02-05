a019d6ba5d0f48a0415d8efaab819d96b7b11e93
juliano <julianokessler@yahoo.com.br>
2023-02-05 11:57:45 -0300

    added curl

--- workstation.sh
+++ workstation.sh
@@ -2,6 +2,16 @@
 apt get update
 apt get upgrade -y 
 
+# manual configs:
+# - konsole
+#  -- ctrl + c for copy, ctrl + v for paste
+#  -- font size
+# - adjust task switcher
+# - adjust task bar
+
+# curl
+sudo apt install curl
+
 # filezilla
 apt get install filezilla -y
 
@@ -9,8 +19,9 @@ apt get install filezilla -y
 sudo apt install keepassxc
 # https://keepassxc.org/download/#linux
 
-# Snowflake (ftp e scp client)
+# Snowflake (ftp and scp client)
 # https://github.com/subhra74/snowflake
+
 # Dowwnload file, then: 
 # sudo apt install ./snowflake-1.0.4-setup-amd64.deb
 
