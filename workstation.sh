1f983b74f7b0d6517b3f4945ebdc5cf10150c995
juliano <julianokessler@yahoo.com.br>
2023-02-12 15:49:16 -0300

    added aws cli

--- workstation.sh
+++ workstation.sh
@@ -102,4 +102,7 @@ sudo apt  install tree
 sudo snap install multipass
 
 # aws cli
-
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+sudo apt install unzip -y
+unzip /tmp/awscliv2.zip -d /tmp
+sudo /tmp/aws/install
