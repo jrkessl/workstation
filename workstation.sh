df0f11a413bace5a4fc121ea3ad769e88ddb3d51
juliano <julianokessler@yahoo.com.br>
2023-02-12 15:19:30 -0300

    garbage cleanup

--- workstation.sh
+++ workstation.sh
@@ -73,8 +73,3 @@ sudo gpasswd -a juliano docker
 
 # nmap
 sudo apt  install nmap -y
-
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
